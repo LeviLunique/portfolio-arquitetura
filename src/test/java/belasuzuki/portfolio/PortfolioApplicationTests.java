960b81168a160be31c3e06fa6e43e5353bf5564e
Levi Lunique <levi.lunique@gmail.com>
2023-09-12 11:51:58 -0300

    First commit

---
+++ src/test/java/belasuzuki/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,16 @@
+package belasuzuki.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
